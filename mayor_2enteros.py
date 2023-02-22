c673931e75fd70af60ea4945dbc061253f792f9b
GeorginaCarvajal <georginacarvajal@colegioguanenta.edu.co>
2023-02-22 07:57:19 -0500

    se agrego el programa en Phyton

---
+++ mayor_2enteros.py
@@ -0,0 +1,19 @@
+#programa para verificar cual de los números enteros es el mayor
+print(------------------------)
+print(----MAYOR 2 ENTEROS ----)
+print(------------------------)
+#input
+x = int(input("digite el valor de x: "))
+x = int(input("digite el valor de x: "))
+
+#processing
+if == y:
+    #output
+    print("los numeros son iguales...")
+else:
+    if x > y:
+    mayor = x
+    else:
+    mayor = y
+    #output
+    print("El mayor entre " + str(x) + " y " + str(ly) + " es " + str(mayor))
